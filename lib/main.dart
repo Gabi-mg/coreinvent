0d44218d3f91ab02a2d977876b3bf9e8d396d46c
Gabriel Muñoz García <gmunoz@encamina.com>
2023-04-23 12:08:55 +0200

    languages

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:coreinvent/core/scheme/color_schemes.dart';
 import 'package:coreinvent/routes.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:coreinvent/di/app_di.dart' as di;
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async{
   await di.init();
@@ -27,16 +29,16 @@ class App extends StatelessWidget {
       routeInformationProvider: _router.routeInformationProvider,
       routeInformationParser: _router.routeInformationParser,
       routerDelegate: _router.routerDelegate,
-      // localizationsDelegates: const [
-      //   AppLocalizations.delegate,
-      //   GlobalMaterialLocalizations.delegate,
-      //   GlobalWidgetsLocalizations.delegate,
-      //   GlobalCupertinoLocalizations.delegate,
-      // ],
-      // supportedLocales: const [
-      //   Locale('es', ''), //Spanish
-      //   // Locale('en', ''), //English
-      // ],
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('es', ''), //Spanish
+        // Locale('en', ''), //English
+      ],
       title: 'Coreinvent',
     );
   }
